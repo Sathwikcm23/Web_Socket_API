c8c7d99c04ceba078fb927b223db5cb37c8d9e5d
Sathwik C M <91399822+Sathwikcm23@users.noreply.github.com>
2023-05-31 17:27:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const app = express()
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
 
+//creates a WebSocket server instance and associates it with the HTTP server
 const wss = new WebSocket.Server({ server:server });
 
+//fires when a client establishes a WebSocket connection
 wss.on('connection', function connection(ws) {
   console.log('A new client Connected!');
   ws.send('Welcome New Client!');
 
+//fires when a message is received from a client  
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
 
+//Iterates over all connected WebSocket clients    
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
